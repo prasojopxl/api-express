c28c2948d8f35425c6f742f315724cd36ecec113
Sapto Prasojo <prasojodesigner@gmail.com>
2022-09-15 03:26:29 +0700

    update

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/products", getProduct);
 router.get('/products/:id', getProductById);
 router.post("/products", saveProduct);
-router.patch("/products", updateProduct);
+router.patch('/products/:id', updateProduct);
 router.delete("/products/:id", deleteProduct);
 
 
